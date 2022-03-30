8e26d5bca97b3fda6ecae9df61ec3ea1d0c4fe86
sammyalwayscode <olorundasamuel2@gmail.com>
2022-03-30 13:01:22 +0100

    Concluding

--- src/Components/Detail/Detail.js
+++ src/Components/Detail/Detail.js
@@ -1,16 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import data from "../Data.json";
 
 const Detail = () => {
+  const { id } = useParams();
+  console.log(data[id - 1]);
+  const newID = data[id - 1];
+
+  console.log("This is the new id", newID);
   return (
     <Container>
-      <BackGround>
-        <img
-          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/B409C2A425D58C32D822EB633C7CAE3DC910DC2FC62D2B1807A0BB092C531E9A/scale?width=1440&aspectRatio=1.78&format=jpeg"
-          alt=""
-        />
-      </BackGround>
-      <h1>Hello</h1>
+      {
+        <div>
+          <BackGround>
+            <img src={newID.backgroundImg} alt="" />
+          </BackGround>
+          <ImageTitle>
+            <img src={newID.titleImg} alt="" />
+          </ImageTitle>
+          <SubTitle> {newID.subTitle} </SubTitle>
+          <Description> {newID.description} </Description>
+        </div>
+      }
     </Container>
   );
 };
@@ -18,8 +30,33 @@ const Detail = () => {
 export default Detail;
 
 const Container = styled.div`
-  height: 100vh;
-  width: 100%;
   position: relative;
+  display: block;
 `;
-const BackGround = styled.div``;
+const BackGround = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+  opacity: 0.7;
+`;
+
+const ImageTitle = styled.div`
+  padding: 24px;
+  padding-bottom: 24px;
+  width: 100%;
+  img {
+    max-width: 600px;
+    min-width: 200px;
+    width: 35vw;
+  }
+`;
+const SubTitle = styled.div`
+  width: 40vw;
+  padding: 24px;
+`;
+const Description = styled.div`
+  padding: 24px;
+  width: 40vw;
+`;
